f9e860723e75bf6ccd4dc8db267985253b4e8cfb
nastiakuzminih2011 <31884485+nastiakuzminih2011@users.noreply.github.com>
2017-09-30 13:47:53 +0300

    Update HW2.py

--- HW2.py
+++ HW2.py
@@ -1,8 +1,12 @@
 a = int ( input() )
 b = int ( input() )
 c = int ( input() )
-if ( a * b == c ) or ( a // b == c ) :
+if ( a * b == c ) :
     print ( "YES" )
 else :
     print ( "NO" )
+if ( a // b == c ) :
+    print ( "yes" )
+else :
+    print ( "no" )
     
