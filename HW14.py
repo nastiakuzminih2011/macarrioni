653003573992e0a5c98eda9a9be2f5ca65efd5c9
nastiakuzminih2011 <31884485+nastiakuzminih2011@users.noreply.github.com>
2018-04-23 00:04:49 +0300

    Update HW14.py

--- HW14.py
+++ HW14.py
@@ -4,7 +4,7 @@ file_list = os.listdir()
 n = 0
 m = []
 for i in file_list:
-    if re.match(r"([\w\d]+\b\s*){2,}", i):
+    if re.match(r"^([a-zA-Zа-яА-Я0-9\(\)\-_]+\b\s*){2,}$", i):
         n += 1
         if i not in m:
             m.append(i)
